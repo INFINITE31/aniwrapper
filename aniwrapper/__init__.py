51278e94f40cc822dcfe0cd70ba8a839ff593ab7
INFINITE_ <69384561+INFINITE31@users.noreply.github.com>
2022-06-19 17:41:01 +0530

    Add files via upload

---
+++ aniwrapper/__init__.py
@@ -0,0 +1,120 @@
+import requests
+from random import choice
+import json
+from PIL import Image
+
+
+base_url = "https://anime-api.hisoka17.repl.co/img"
+endpoints = ["slap","kiss","cuddle","hug","kill","wink","pat"]
+
+class anime():
+    def __init__(self):
+        super().__init__()
+
+    def kiss(link:bool):
+        r = requests.get(f"{base_url}/kiss")
+
+        if link == True:
+            link = r.json()["url"]
+            return link
+        if link == False:
+
+            link = r.json()["url"]
+            content = requests.get(link).content
+            return content
+
+    def slap(link:bool):
+        r = requests.get(base_url+"/slap")
+
+        if link == True:
+            link = r.json()["url"]
+            return link
+        if link == False:
+            link = r.json()["url"]
+            content = requests.get(link).content
+            return content
+
+    def kill(link:bool):
+        r = requests.get(base_url+"/kill")
+
+        if link == True:
+            link = r.json()["url"]
+            return link
+        if link == False:
+            link = r.json()["url"]
+            content = requests.get(link).content
+            return content
+
+    def hug(link:bool):
+        r = requests.get(base_url+"/hug")
+
+        if link == True:
+            link = r.json()["url"]
+            return link
+        if link == False:
+            link = r.json()["url"]
+            content = requests.get(link).content
+            return content
+
+    def wink(link:bool):
+        r = requests.get(base_url+"/wink")
+
+        if link == True:
+            link = r.json()["url"]
+            return link
+        if link == False:
+            link = r.json()["url"]
+            content = requests.get(link).content
+            return content
+
+    def pat(link:bool):
+        r = requests.get(base_url+"/pat")
+
+        if link == True:
+            link = r.json()["url"]
+            return link
+        if link == False:
+            link = r.json()["url"]
+            content = requests.get(link).content
+            return content
+
+    def cuddle(link:bool):
+        r = requests.get(base_url+"/cuddle")
+
+        if link == True:
+            link = r.json()["url"]
+            return link
+        if link == False:
+            link = r.json()["url"]
+            content = requests.get(link).content
+            return content
+
+class nsfw():
+    def __init__(self):
+        super().__init__()
+    
+
+    def hentai(link:bool):
+        r = requests.get(base_url+"/nsfw/hentai")
+
+        if link == True:
+            link = r.json()["url"]
+            return link
+        if link == False:
+            link = r.json()["url"]
+            content = requests.get(link).content
+            return content
+
+    def boobs(link:bool):
+        r = requests.get(base_url+"/nsfw/boobs")
+
+        if link == True:
+            link = r.json()["url"]
+            return link
+        if link == False:
+            link = r.json()["url"]
+            content = requests.get(link).content
+            return content
+            
+def help():
+    print(endpoints)
--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
-from setuptools import setup
+from setuptools import setup , find_packages
 
 classifiers = [
-    'Development Status :: 3',
-    'Intended Audience :: Anime',
+    'Development Status :: 1 - Planning',
+    'Intended Audience :: Developers',
     'Operating System :: Microsoft :: Windows :: Windows 10',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.8'
+    'Programming Language :: Python :: 3'
 ]
 
 setup(
     name='aniwrapper',
-    version='0.0.1a',
+    version='1.0.0a',
     description='a python api wrapper for the anime image generator api',
-    long_description=open('README.md').read() + '\n\n' +
-    open('CHANGELOG.md').read(),
+    long_description=open('README.md').read(),
+    long_description_content_type = 'text/markdown',
     url="https://github.com/infinite31/aniwrapper",
     author="INFINITE_.",
     author_email='work4infinite@gmail.com',
     license='MIT',
     classifiers=classifiers,
     keywords=['python', 'image', 'anime', 'api', 'wrapper', 'py'],
-    install_requires=['requests']
+    install_requires=['requests'],
+    packages=find_packages()
 )
+
